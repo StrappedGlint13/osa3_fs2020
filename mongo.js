d7e30c46892e77fbe072b96d9c7b6351f05a155a
StrappedGlint13 <matiasbrax@gmail.com>
2020-06-09 18:02:00 +0300

    3.12 done

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0-rlbpx.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+})
+
+if (process.argv.length === 3) {
+console.log(`phonebook:`)
+Person
+  .find({})
+  .then(persons => {
+    persons.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+       
+    })
+    mongoose.connection.close()
+    process.exit(1)
+  })
+}
+
+if (process.argv.length > 3) {
+person.save().then(response => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
+}
