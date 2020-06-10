0c74428401d04974f659b5f3ce5387a4076664e7
StrappedGlint13 <matiasbrax@gmail.com>
2020-06-10 18:53:52 +0300

    3.15 done

--- index.js
+++ index.js
@@ -71,10 +71,14 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-
-	response.status(204).end()
+	Person.findByIdAndRemove(request.params.id)
+	.then(note => {
+		response.json(note)
+	  })
+	  .catch(error => {
+		console.log(error)
+		response.status(404).end()
+	  })
 })
 
 
